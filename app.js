8ffee5da785ef4a64075c9798a6fd4fd29ac5a71
Bello Ahmed <bello.ahmed2001@gmail.com>
2024-04-20 16:50:06 +0100

    Second commit

--- app.js
+++ app.js
@@ -3,22 +3,17 @@ const express = require('express');
 const cors = require('cors')
 const app = express();
 const bodyParser = require('body-parser');
-const router = require('./routes/routes')
-const path = require('path')
-const https = require('https');
+const router = require('./routes/routes');
 
 
 const port = process.env.PORT || 3001;
 app.use(cors())
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'Frontend')))
 
 
 // Call the routers 
 app.use(router);
-// app.get('/', (req, res) => {
-//     res.status(200).sendFile(path.join(__dirname, 'Frontend', 'index.html'));
-// })
+
 
 
 // Start the server
